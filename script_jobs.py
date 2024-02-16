91b3393c6d1e5b09a86fda0a3838c67097a7bbc8
Chitradeep Saha <139892421+deephysics1729@users.noreply.github.com>
2024-02-17 02:58:41 +0530

    Update script_jobs.py

--- script_jobs.py
+++ script_jobs.py
@@ -1,16 +1,15 @@
+# A script for generating LaTeX compatible code for the 'Jobs and Opportunities' section of INDUS Newsletters
+# To know more about INDUS: https://sites.google.com/view/indus-solphys/home
+# Code written by Chitradeep Saha, CESSI, IISER Kolkata
+# Redirect your queries to chitrodeephysics1729@gmail.com
+
 import re
 import pandas as pd
-import math
 
-def convert_google_sheet_url(url):
-    # Regular expression to match and capture the necessary part of the URL
-    pattern = r'https://docs\.google\.com/spreadsheets/d/([a-zA-Z0-9-_]+)(/edit#gid=(\d+)|/edit.*)?'
 
-    # Replace function to construct the new URL for CSV export
-    # If gid is present in the URL, it includes it in the export URL, otherwise, it's omitted
-    replacement = lambda m: f'https://docs.google.com/spreadsheets/d/{m.group(1)}/export?' + (f'gid={m.group(3)}&' if m.group(3) else '') + 'format=csv'
-
-    # Replace using regex
+def convert_spreadsheet_url(url):    
+    pattern = r'https://docs\.google\.com/spreadsheets/d/([a-zA-Z0-9-_]+)(/edit#gid=(\d+)|/edit.*)?'    
+    replacement = lambda m: f'https://docs.google.com/spreadsheets/d/{m.group(1)}/export?' + (f'gid={m.group(3)}&' if m.group(3) else '') + 'format=csv'    
     new_url = re.sub(pattern, replacement, url)
     return new_url
 
@@ -19,8 +18,9 @@ def latexscript():
     issue = input('Mention ddmmyyyy of the newsletter issue\n')
     issue=float(issue)
     url = 'https://docs.google.com/spreadsheets/d/1pkfSimtd0-Qu-5HRD-W2g0L0NpBfZric5ExJ2GBVIyo/edit#gid=1284969652'
-    new_url = convert_google_sheet_url(url)
+    new_url = convert_spreadsheet_url(url)
     df= pd.read_csv(new_url)
+    
     for i in range(0,len(df)):
         if df.Issue[i]==issue:
             if str(df.Position[i])!='nan':
@@ -30,7 +30,6 @@ def latexscript():
         else:
             print('No entry found!')
 
-    
-    
+
 if __name__ == "__main__":
     latexscript()
